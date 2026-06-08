1023dd477cb66387d4712e70eb609edf8e20ebff
Enzo Costa Nunes Cipriano <enzocngr@gmail.com>
2026-06-08 14:25:12 -0300

    Básico em assembly

---
+++ ASM/hello_world.asm
@@ -0,0 +1,19 @@
+; Primeiro Codigo em ASSEMBLY
+
+section .text
+
+section .data
+	msg db "hello world", 0x00
+	msglen equ $ - msg
+
+global _start
+_start:
+	mov eax,0x04
+	mov ebx,0x01
+	mov ecx,msg
+	mov edx,msglen
+	int 0x80
+
+	mov eax,0x01
+	mov ebx,0
+	int 0x80
